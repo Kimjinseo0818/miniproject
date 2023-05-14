faa5540b1cb492da7e72ef3a6b3cd51f26737739
KimJinseo <jinseokim33@gmail.com>
2023-05-14 22:00:15 +0900

    Create Update

--- order.c
+++ order.c
@@ -31,11 +31,19 @@ void listMenu(Menu *p[], int count) {
 }
 
 void readMenu(Menu p) {
-
+	printf("%s %d\n", s.name, s.price);
 }
 
 int updateMenu(Menu *p) {
-
+	printf("음식 이름은? ");
+        getchar();
+        fgets(s->name, 100, stdin);
+        s->name[strlen(s->name) - 1] = '\0';
+        printf("가격은? ");
+        scanf("%d", &s->price);
+        printf("요일은? (월:0, 화:1, 수:2, 목:3, 금:4)");
+        scanf("%s", s->day);
+        return 1;
 }
 
 void deleteMenu(Menu *p[], char fname[100]) {
