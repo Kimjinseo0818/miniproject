8dd3c1ba557007c9d2ff2b9846024f60cd0d447b
Kimjinseo0818 <130707940+Kimjinseo0818@users.noreply.github.com>
2023-05-22 14:09:58 +0900

    Update main.c

--- main.c
+++ main.c
@@ -23,44 +23,40 @@ int main() {
   		printf("3. 수정 ");
   		printf("4. 삭제 ");
   		printf("5. 파일저장 ");
-  		printf("6. 물품검색 ");
-  		printf("7. 사이즈분류 ");
   		printf("0. 종료 >> ");
   		scanf("%d", &menu);
 
 		if (menu == 0)
                         break;
                 if (menu == 1) {
-
                         if(count > 0) {
-                                listItem(s, index);
+                                listOrder(p, index);
                         } else {
-                                printf("데이터가 없습니다.\n");
+                                printf("주문이내역이 없습니다.\n");
                         }
                 } else if (menu == 2) {
-                        s[index] = (Item *)malloc(sizeof(Item));
-                        count += addItem(s[index++]);
+                        s[index] = (Order *)malloc(sizeof(Order));
+                        count += addOrder(p[index++]);
                 } else if (menu == 3) {
-                        listItem(s, index);
-                        printf("번호는 (취소: 0)? ");
+                        listOrder(p, index);
+                        printf("주문 번호는 (취소: 0)? ");
                         scanf("%d", &num);
                         if (num == 0) {
                                 printf("=>취소됨!\n");
                                 continue;
                         } else {
-                                updateItem(s[num-1]);
+                                updateOrder(p[num-1]);
                         }
                 } else if (menu == 4) {
-                        listItem(s, index);
-
-                        printf("\n번호는 (취소: 0)? ");
+                        listOrder(p, index);
+                        printf("\n취소할 주문의 주문 번호는 (취소: 0)? ");
                         scanf("%d", &num);
                         if (num == 0) {
                                 continue;
                         }
-                        deleteItem(s, num);
+                        deleterOrder(s, num);
                 } else if (menu == 5) {
-                        saveData(s, count);
+                        saveOrder(p, count);
                 }
 	}
 	printf("종료!\n");
