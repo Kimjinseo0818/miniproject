72976cd0c15ee1fa9a988fd58c13e1b3e56d41d8
Kimjinseo0818 <130707940+Kimjinseo0818@users.noreply.github.com>
2023-05-22 15:53:02 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,18 +4,34 @@
 
 int main() {
 	Order p[100];
-	Menu s[20];
+	Menu s[50];
 	int count, menu;
         int index = 0;
         int num;
 	int day;
+	int nday;
 	int menu;
+	int ncount;
+	int acount;
 	printf("오늘 요일은? (월:0, 화:1, 수:2, 목:3, 금:4) ");
 	scanf("%d", &day);
-	
-	count = loadOrder(s);
-        index = count;
-
+	acount = loadData(s);
+	ncount = loadMenu(s, day);
+	FILE *file;
+	file = fopen("day.txt", "rt");
+	fscanf(file, "%d", &nday);
+	fclose(file);
+	if (nday == day) {
+		count = loadOrder(p, ncount);
+		index = count;
+	} else {
+		count = 0;
+		index = count;
+	}
+	FILE *file;
+	file = fopen("day.txt", "wt");
+	fprintf(file, "%d", &nday);
+	fclose(file);
 	while(1) {	
   		printf("\n*** 재고 확인 ***\n");
   		printf("1. 조회 ");
@@ -32,11 +48,12 @@ int main() {
                         if(count > 0) {
                                 listOrder(p, index);
                         } else {
-                                printf("주문이내역이 없습니다.\n");
+                                printf("주문 내역이 없습니다.\n");
                         }
                 } else if (menu == 2) {
+			listMenu(s, acount, day);
                         s[index] = (Order *)malloc(sizeof(Order));
-                        count += addOrder(p[index++]);
+                        count += addOrder(p[index++], s, ncount, day);
                 } else if (menu == 3) {
                         listOrder(p, index);
                         printf("주문 번호는 (취소: 0)? ");
@@ -45,7 +62,8 @@ int main() {
                                 printf("=>취소됨!\n");
                                 continue;
                         } else {
-                                updateOrder(p[num-1]);
+				listMenu(s, acount, day);
+                                updateOrder(p[num-1], s, ncount, day);
                         }
                 } else if (menu == 4) {
                         listOrder(p, index);
@@ -56,7 +74,7 @@ int main() {
                         }
                         deleterOrder(s, num);
                 } else if (menu == 5) {
-                        saveOrder(p, count);
+                        saveOrder(p, count, ncount);
                 }
 	}
 	printf("종료!\n");
